1d8cb678a8a9bd5e97b4e2b55918d4858dc524f4
Shlomo Kofman <skofman@indeed.com>
2020-02-21 15:38:57 -0800

    24

--- src/024/main.go
+++ src/024/main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+
+	"../helpers"
 )
 
 func main() {
-	fmt.Println(orderNumbers())
+	fmt.Println(orderNumbers(1000000, 10))
 }
 
-func orderNumbers() []int {
+func orderNumbers(order int, digits int) string {
+	arr := []int{}
+	for i := 0; i < digits; i++ {
+		arr = append(arr, i)
+	}
 
+	length := len(arr)
+	result := ""
+	for len(result) < length {
+		permutations := helpers.Factorial(len(arr))
+		index := (order - 1) / (permutations / len(arr))
+		for index >= len(arr) {
+			index -= len(arr)
+		}
+		result += strconv.Itoa(arr[index])
+		arr = helpers.RemoveSliceIntElement(arr, index)
+	}
+
+	return result
 }
