78ba035396530371343aa2ce77da3fdeab2b4016
Shlomo Kofman <skofman@indeed.com>
2020-02-25 09:27:03 -0800

    15

--- src/015/main.go
+++ src/015/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	"../helpers"
 )
 
 func main() {
@@ -9,5 +11,31 @@ func main() {
 }
 
 func path(num int) int {
-	return 0
+	rows := num + 1
+	cols := num + 1
+	row := []int{1}
+	arr := [][]int{row}
+	holder := make([]int, rows*cols)
+	holder[0] = 1
+	for len(arr) < 2*num+1 {
+		newRow := []int{}
+		for _, r := range arr[len(arr)-1] {
+			if r%cols != 0 {
+				if !helpers.SliceContainsInt(newRow, r+1) {
+					newRow = append(newRow, r+1)
+				}
+				holder[r] += holder[r-1]
+
+			}
+			if r < num*rows+1 {
+				if !helpers.SliceContainsInt(newRow, r+rows) {
+					newRow = append(newRow, r+rows)
+				}
+				holder[r+rows-1] += holder[r-1]
+			}
+		}
+		arr = append(arr, newRow)
+	}
+
+	return holder[len(holder)-1]
 }
