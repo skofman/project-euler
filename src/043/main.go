e36204a9ae7872b02c25c69797ab187c5f7fc73b
Shlomo Kofman <skofman@indeed.com>
2020-03-03 02:08:15 -0600

    43

--- src/043/main.go
+++ src/043/main.go
@@ -3,121 +3,84 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"strings"
-
-	"../helpers"
 )
 
 func main() {
-	// fmt.Println(subStringDiv())
-	fmt.Println(getAllPandigitals([]int{1, 2, 0}))
+	fmt.Println(subStringDiv())
 }
 
-func getAllPandigitals(s []int) []int {
-	result := []int{}
+func getAllPandigitals(n int, A []int) []string {
+	result := []string{}
+	c := []int{}
 
-	index := 0
-	for index < len(s)-1 {
-		startNum := s[index]
-		result = append(result, generateNumber(s))
-		nextNumber := moveNumber(s[index])
-		for startNum != nextNumber {
-			swapIndex := findIndex(s, nextNumber)
-			swapNumbers(s, index, swapIndex)
-			result = append(result, generateNumber(s))
-			swapNumbers(s, index, swapIndex)
-			nextNumber = moveNumber(nextNumber)
-			if index == 0 && nextNumber == 0 {
-				nextNumber = 1
+	for i := 0; i < n; i++ {
+		c = append(c, 0)
+	}
+
+	str := generateString(A)
+	result = append(result, str)
+
+	i := 0
+	for i < n {
+		if c[i] < i {
+			if i%2 == 0 {
+				A = swap(A, 0, i)
+			} else {
+				A = swap(A, c[i], i)
 			}
+			str := generateString(A)
+			result = append(result, str)
+			c[i]++
+			i = 0
+		} else {
+			c[i] = 0
+			i++
 		}
-		index++
 	}
 
 	return result
 }
 
-func swapNumbers(s []int, index int, swapIndex int) {
-	temp := s[index]
-	s[index] = s[swapIndex]
-	s[swapIndex] = temp
-}
+func generateString(arr []int) string {
+	str := ""
 
-func findIndex(s []int, num int) int {
-	for i, item := range s {
-		if item == num {
-			return i
-		}
+	for _, num := range arr {
+		str += strconv.Itoa(num)
 	}
 
-	return -1
-}
-
-func moveNumber(num int) int {
-	if num == 2 {
-		return 0
-	}
-
-	return num + 1
-}
-
-func generateNumber(s []int) int {
-	strArr := []string{}
-	for _, item := range s {
-		strArr = append(strArr, strconv.Itoa(item))
-	}
-	strNum := strings.Join(strArr, "")
-	num, _ := strconv.Atoi(strNum)
-
-	return num
+	return str
 }
 
 func subStringDiv() int {
-	start := 1023456789
-	end := 9876543210
+	numbers := getAllPandigitals(10, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0})
+	primes := []int{2, 3, 5, 7, 11, 13, 17}
 	sum := 0
-	for i := start; i <= end; i++ {
-		if isPandigital(i) && isDiv(i) {
-			sum += i
+
+	for _, num := range numbers {
+		for i := 1; i <= 7; i++ {
+			testNum, _ := strconv.Atoi(num[i : i+3])
+			div := true
+			if testNum%primes[i-1] != 0 {
+				div = false
+			}
+			if !div {
+				break
+			}
+			if i == 7 {
+				number, _ := strconv.Atoi(num)
+				sum += number
+			}
 		}
+
 	}
 
 	return sum
 }
 
-func isDiv(num int) bool {
-	strNum := strconv.Itoa(num)
-	for i := 2; i <= 8; i++ {
-		subStr := strNum[i : i+3]
-		subNum, _ := strconv.Atoi(subStr)
-		if !checkDiv(subNum) {
-			return false
-		}
-	}
-	return true
-}
+func swap(arr []int, i1 int, i2 int) []int {
+	temp := arr[i1]
+	arr[i1] = arr[i2]
+	arr[i2] = temp
 
-func checkDiv(num int) bool {
-	for i := 2; i < num/2; i++ {
-		if num%i == 0 {
-			return true
-		}
-	}
-	return false
-}
-
-func isPandigital(num int) bool {
-	arr := strings.Split(strconv.Itoa(num), "")
-	max := 0
-	for i := 0; i < len(arr); i++ {
-		numArr, _ := strconv.Atoi(arr[i])
-		if numArr > max {
-			max = numArr
-		}
-	}
-
-	if len(strconv.Itoa(num)) == len(helpers.UniqueStringSlice(arr)) && !helpers.SliceContainsString(arr, "0") && max == len(strconv.Itoa(num)) {
-		return true
-	}
-	return false
+	return arr
 }
