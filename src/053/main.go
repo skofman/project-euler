b6e4e56bb75f0597804ac9dfeeb31b45af6bfeec
Shlomo Kofman <skofman@indeed.com>
2020-03-04 00:34:19 -0600

    53

--- src/053/main.go
+++ src/053/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-
-	"../helpers"
 )
 
 func main() {
@@ -11,16 +9,52 @@ func main() {
 }
 
 func findValues() int {
-	count := 0
+	total := 0
 
-	for i := 1; i <= 100; i++ {
+	for i := 10; i <= 100; i++ {
 		for j := 1; j <= i; j++ {
-			value := float64(helpers.Factorial(i)) / (float64(helpers.Factorial(j)) * float64(helpers.Factorial(i-j)))
-			if value > 1000000.0 {
-				count++
+			if isMill(i, j) {
+				total++
 			}
 		}
 	}
 
-	return count
+	return total
+}
+
+func isMill(n int, r int) bool {
+	diff := n - r
+	if diff == 0 {
+		return false
+	}
+	startTop := float64(n)
+	endTop := float64(r)
+	startBottom := float64(n - r)
+	endBottom := float64(1)
+	result := float64(1)
+	action := "*"
+	for true {
+		if startTop == endTop && result <= 1000000 {
+			return false
+		}
+		if startBottom == endBottom && result > 1000000 {
+			return true
+		}
+		if action == "*" {
+			result *= startTop
+			startTop--
+			if result > 1000000 {
+				action = "/"
+			}
+		} else if action == "/" {
+			result /= startBottom
+			startBottom--
+			if result <= 1000000 {
+				action = "*"
+			}
+		}
+
+	}
+
+	return true
 }
